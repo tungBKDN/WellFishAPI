b1b737df91f611f6f545f86ce1e9a654c9745018
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2024-08-06 23:17:20 +0700

    bugs: cannot fix CORS on deploy server

--- corsConfig.js
+++ corsConfig.js
@@ -1,19 +1,19 @@
 const cors = require('cors');
 
 const indexCorsOptions = {
-    origin: ['http://localhost:3000', 'http://54.254.178.139:3000'],
+    origin: ['http://localhost:3000', 'http://54.254.178.139:3000', 'http://localhost:3333', 'http://54.254.178.139:3333'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Admin-Header', 'X-User-Header', 'Cookie']
 };
 
 const adminCorsOptions = {
-    origin: ['http://localhost:3000', 'http://54.254.178.139:3000'],
+    origin: ['http://localhost:3000', 'http://54.254.178.139:3000', 'http://localhost:3333', 'http://54.254.178.139:3333'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Admin-Header'],
 };
 
 const userCorsOptions = {
-    origin: ['http://localhost:3000', 'http://54.254.178.139:3000'],
+    origin: ['http://localhost:3000', 'http://54.254.178.139:3000', 'http://localhost:3333', 'http://54.254.178.139:3333'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Header'],
 };
